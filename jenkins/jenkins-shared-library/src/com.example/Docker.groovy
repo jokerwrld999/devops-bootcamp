bf5ad7d637a4674bf1da74ba82bb21403e4145b9
jokerwrld999 <ifalaleev49@gmail.com>
2023-05-03 09:29:50 +0000

    [jenkins] Extract logic into Groovy classes

---
+++ jenkins/jenkins-shared-library/src/com.example/Docker.groovy
@@ -0,0 +1,20 @@
+#!/usr/bin/env groovy
+
+package com.example
+
+class Docker implements Serializable {
+    def script
+
+    Docker(script) {
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName) {
+        script.echo "Building Docker image..."
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-creds', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+            script.sh "docker build -t $imageName ."
+            script.sh "echo $script.DOCKER_PASSWORD | docker login -u $script.DOCKER_USERNAME --password-stdin "
+            script.sh "docker push $imageName"
+        }
+    }
+}
--- jenkins/jenkins-shared-library/vars/buildImage.groovy
+++ jenkins/jenkins-shared-library/vars/buildImage.groovy
@@ -1,10 +1,7 @@
 #!/usr/bin/env groovy
 
-def call() {
-    echo "Building Docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-creds', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-        sh "docker build -t $REPOSITORY ."
-        sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin "
-        sh "docker push $REPOSITORY"
-    }
+import com.example.Docker
+
+def call(String imageName) {
+    return new Docker(this).buildDockerImage(imageName)
 }
