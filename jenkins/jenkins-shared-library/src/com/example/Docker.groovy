92fe0952ae90ec7d69f58550903acf44dcbfeb39
jokerwrld999 <ifalaleev49@gmail.com>
2023-05-03 10:47:24 +0000

    [jenkins] Separate docker steps

--- jenkins/jenkins-shared-library/src/com/example/Docker.groovy
+++ jenkins/jenkins-shared-library/src/com/example/Docker.groovy
@@ -10,10 +10,18 @@ class Docker implements Serializable {
 
     def buildDockerImage(String imageName) {
         script.echo "Building Docker image..."
+        script.sh "docker build -t $imageName ."
+    }
+
+    def dockerLogin() {
+        script.echo "Login to DockerHub..."
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-creds', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-            script.sh "docker build -t $imageName ."
             script.sh "echo $script.DOCKER_PASSWORD | docker login -u $script.DOCKER_USERNAME --password-stdin "
-            script.sh "docker push $imageName"
         }
+    }
+
+    def pushImage(String imageName) {
+        script.echo "Pushing Docker image..."
+        script.sh "docker push $imageName"
     }
 }
