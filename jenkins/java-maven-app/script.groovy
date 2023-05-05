79c4a75e0ac2b92ef24e2098e7c8a88dc04f4bfa
jokerwrld999 <ifalaleev49@gmail.com>
2023-05-05 18:18:24 +0000

    [jenkins] Deploy to EC2

--- jenkins/java-maven-app/script.groovy
+++ jenkins/java-maven-app/script.groovy
@@ -1,5 +1,11 @@
 def deployApp() {
     echo "Deploying app on the server..."
+    def dockerStop ="docker stop $appName"
+    def dockerRun = "docker run --rm -d --name $appName -p 4444:8080 $imageName:${versionTag}"
+    sshagent(credentials: ['ec2-private-key']) {
+      sh "ssh -o StrictHostKeyChecking=no ec2-user@18.197.109.37 ${dockerStop}"
+      sh "ssh -o StrictHostKeyChecking=no ec2-user@18.197.109.37 ${dockerRun}"
+    }
 }
 
 return this
